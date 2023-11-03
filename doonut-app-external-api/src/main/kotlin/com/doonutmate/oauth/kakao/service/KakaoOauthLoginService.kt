9761a278da28cef421e4bb4c0f4a9b0b4c6463e7
jickDo <pocard0117@naver.com>
2023-11-03 12:31:39 +0900

    return 추가

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/kakao/service/KakaoOauthLoginService.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/kakao/service/KakaoOauthLoginService.kt
@@ -4,11 +4,8 @@ import com.doonutmate.doonut.member.model.Member
 import com.doonutmate.doonut.member.model.OauthType
 import com.doonutmate.doonut.member.service.MemberBusinessService
 import com.doonutmate.oauth.kakao.dto.KakaoTokenRequest
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.lang.Exception
-import javax.lang.model.type.ErrorType
 
 @Service
 @Transactional
@@ -20,7 +17,7 @@ class KakaoOauthLoginService(
     fun kakaoLoginPreventDuplication(tokenRequest: KakaoTokenRequest, oauthType: OauthType): Member? {
         val savedId = kakaoAccessClientLoginService.getKakaoUserId(tokenRequest)
         return memberBusinessService.getByOauthId(savedId.findMemberId())
-            ?: signUpNewMember(tokenRequest, oauthType) ?:
+            ?: return signUpNewMember(tokenRequest, oauthType)
     }
 
     fun signUpNewMember(tokenRequest: KakaoTokenRequest, oauthType: OauthType): Member? {

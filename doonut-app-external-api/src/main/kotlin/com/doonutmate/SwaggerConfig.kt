2e9020b504ffb8e619c939f905d40c69b3933ae0
jickDo <pocard0117@naver.com>
2023-11-22 12:14:22 +0900

    swagger 헤더에 토큰을 담을수 있게 변경

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/SwaggerConfig.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/SwaggerConfig.kt
@@ -3,7 +3,8 @@ package com.doonutmate
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
-import org.springdoc.core.models.GroupedOpenApi
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -12,11 +13,22 @@ class SwaggerConfig {
     @Bean
     fun customOpenAPI(): OpenAPI {
         return OpenAPI()
-            .components(Components())
+            .components(
+                Components().addSecuritySchemes(
+                    "bearerAuth",
+                    SecurityScheme().apply {
+                        name = "bearerAuth"
+                        type = SecurityScheme.Type.HTTP
+                        scheme = "bearer"
+                        bearerFormat = "JWT"
+                    },
+                ),
+            )
             .info(
                 Info().title("doonut-app-exteranl-api")
                     .description("")
                     .version("1.0"),
             )
+            .addSecurityItem(SecurityRequirement().addList("bearerAuth"))
     }
 }

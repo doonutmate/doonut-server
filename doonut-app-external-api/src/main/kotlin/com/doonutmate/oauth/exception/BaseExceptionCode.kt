0208138042a8beab72bb3958754391ef8308fca5
jickDo <pocard0117@naver.com>
2023-11-22 12:21:06 +0900

    커스텀 예외처리 enum에러 3가지 생성

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/exception/BaseExceptionCode.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/exception/BaseExceptionCode.kt
@@ -1,4 +1,12 @@
 package com.doonutmate.oauth.exception
 
-enum class BaseExceptionCode {
+import org.springframework.http.HttpStatus
+
+enum class BaseExceptionCode(val httpStatusCode: Int, val message: String) {
+
+    REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "요청이 존재하지 않습니다."),
+
+    AUTHORIZATION_HEADER_NULL(HttpStatus.BAD_REQUEST.value(), "인증 헤더가 null입니다."),
+
+    INVALID_TOKEN_PREFIX(HttpStatus.UNAUTHORIZED.value(), "Bearer값이 아닙니다"),
 }

72c26a28f8996f15a358cb06b2051b57e92ce81a
jickDo <pocard0117@naver.com>
2024-01-21 17:03:24 +0900

    feat:domain entitiy & repo

--- doonut-core/src/main/java/com/doonutmate/doonut/image/repository/ImageRepository.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/image/repository/ImageRepository.java
@@ -1,2 +1,7 @@
-package com.doonutmate.doonut.image.repository;public interface ImageRepository {
+package com.doonutmate.doonut.image.repository;
+
+import com.doonutmate.doonut.image.entity.ImageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
 }

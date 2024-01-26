18af291175a7f6d194957db7540d1c3294c25e2e
jickDo <pocard0117@naver.com>
2024-01-26 13:36:51 +0900

    refactor: exceptionHandler 구조 개선

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/exception/ErrorResponse.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/exception/ErrorResponse.kt
@@ -1,16 +1,12 @@
 package com.doonutmate.oauth.exception
 
+import org.springframework.http.HttpStatus
+
 data class ErrorResponse(
     val status: Int,
     val error: String,
     val message: String,
     val path: String,
-)
-
-// val errorResponse = ErrorResponse(
-//    timestamp = LocalDateTime.now(),
-//    status = HttpStatus.BAD_REQUEST.value(),
-//    error = "Bad Request",
-//    message = exception.message,
-//    path = request.requestURI,
-// )
+) {
+    constructor(httpStatus: HttpStatus, message: String, path: String) : this(httpStatus.value(), httpStatus.name, message, path)
+}
--- doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/exception/ExceptionHandler.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/exception/ExceptionHandler.kt
@@ -7,48 +7,52 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
+private const val UNKNOWN_ERROR = "Unknown error"
+
 @RestControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
 
-    /*
- * Custom Exception
- */
+    @ExceptionHandler(BaseException::class)
+    fun handleBaseException(exception: BaseException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        val errorMessage = getErrorMessage(exception)
+        val errorLocation = getErrorLocation(exception)
 
-    @ExceptionHandler(IllegalArgumentException::class, IllegalStateException::class)
+        logger.warn("BaseException occurred: $errorMessage , Location: $errorLocation")
+        return createErrorResponse(HttpStatus.BAD_REQUEST, errorMessage, request)
+    }
+
+    @ExceptionHandler(RuntimeException::class)
     fun handleBadRequestException(exception: RuntimeException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
-        val errorMessage = exception.message ?: "Unknown error"
-        val errorLocation = exception.stackTrace.firstOrNull()
+        val errorMessage = getErrorMessage(exception)
+        val errorLocation = getErrorLocation(exception)
 
-        logger.error("Error occurred: $errorMessage , Location: $errorLocation")
+        logger.error("RuntimeException occurred: $errorMessage , Location: $errorLocation")
+        return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage, request)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleBadRequestException(exception: Exception, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        val errorMessage = getErrorMessage(exception)
+        val errorLocation = getErrorLocation(exception)
+
+        logger.error("Exception occurred: $errorMessage , Location: $errorLocation")
+        return createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage, request)
+    }
+
+    private fun getErrorMessage(exception: Exception) = exception.message ?: UNKNOWN_ERROR
+
+    private fun getErrorLocation(exception: Exception) = exception.stackTrace.firstOrNull()
+
+    private fun createErrorResponse(
+        httpStatus: HttpStatus,
+        errorMessage: String,
+        request: HttpServletRequest,
+    ): ResponseEntity<ErrorResponse> {
         val errorResponse = ErrorResponse(
-            status = HttpStatus.BAD_REQUEST.value(),
-            error = HttpStatus.BAD_REQUEST.name,
-            message = exception.message ?: "Unknown error",
+            httpStatus,
+            errorMessage,
             request.requestURI,
         )
         return ResponseEntity.badRequest().body(errorResponse)
     }
-
-//    @ExceptionHandler(BaseException::class)
-//    fun handleBaseException(exception: BaseException): ResponseEntity<ApiResponse<Unit>> {
-//        logger.warn(exception.message, exception)
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//            .body(ApiResponse.error(exception.message))
-//    }
-//
-//    @ExceptionHandler(RuntimeException::class)
-//    fun handleRuntimeException(exception: RuntimeException): ResponseEntity<ApiResponse<Unit>> {
-//        logger.warn("RuntimeException: ${exception.message}", exception)
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//            .body(ApiResponse.error(exception.message))
-//    }
-//
-//    @ExceptionHandler(Exception::class)
-//    fun handleGlobalException(exception: Exception): ResponseEntity<ApiResponse<Unit>> {
-//        logger.error("예상하지 못한 에러가 발생했습니다. ${exception.message}", exception)
-//        logger.error(exception.stackTrace[0].toString())
-//        return ResponseEntity
-//            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-//            .body(ApiResponse.error(exception.message, exception.stackTrace[0]))
-//    }
 }

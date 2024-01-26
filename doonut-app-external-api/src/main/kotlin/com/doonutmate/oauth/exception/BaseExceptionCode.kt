1d370c740c0b4424a1849f2c9f2a7d45df45be28
yeongun <gjduddns@gmail.com>
2024-01-26 14:38:22 +0900

    refactor: 주석을 javadoc 스타일로 변경

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/exception/BaseExceptionCode.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/exception/BaseExceptionCode.kt
@@ -4,19 +4,19 @@ import org.springframework.http.HttpStatus
 
 enum class BaseExceptionCode(val httpCode: HttpStatus, val message: String) {
 
-    /*
+    /**
      * 400 BAD_REQUEST: 잘못된 요청
+     *
+     * @see org.springframework.http.HttpStatus.BAD_REQUEST
      */
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-
-    // TODO 추후 에러 추가
-
     AUTHORIZATION_HEADER_NULL(HttpStatus.BAD_REQUEST, "인증 헤더가 null입니다."),
-
     INVALID_TOKEN_PREFIX(HttpStatus.UNAUTHORIZED, "Bearer값이 아닙니다"),
 
-    /*
-    * 500 INTERNAL SERVER ERROR: 내부 서버 오류
-    */
+    /**
+     * 500 INTERNAL SERVER ERROR: 내부 서버 오류
+     *
+     * @see org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+     */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다."),
 }

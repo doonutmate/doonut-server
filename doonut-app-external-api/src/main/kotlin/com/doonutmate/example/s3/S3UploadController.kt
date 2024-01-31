d254e25b9ea6aafaecc9ffaaddb7a5e776dba818
yeongun <gjduddns@gmail.com>
2024-01-31 23:52:32 +0900

    feat: 스웨거상에서 MultipartFile을 업로드할 수 있도록 구현

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/example/s3/S3UploadController.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/example/s3/S3UploadController.kt
@@ -1,15 +1,22 @@
 package com.doonutmate.example.s3
 
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
+@Tag(name = "S3 업로드 example", description = "S3 업로드 테스트용 API")
 class S3UploadController(
     private val s3UploadService: S3UploadService,
 ) {
-    @PostMapping("/upload")
+    @PostMapping(
+        value = ["/upload"],
+        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE],
+    )
     fun testUpload(@RequestPart("multipartFile") multipartFile: MultipartFile) {
         s3UploadService.saveFile(multipartFile)
     }

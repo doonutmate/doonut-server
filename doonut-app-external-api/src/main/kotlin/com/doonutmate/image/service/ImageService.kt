e3e1b37d3ead87ffb836024c3d11284944881165
jickDo <95222741+jickDo@users.noreply.github.com>
2024-02-11 21:32:35 +0900

    Feature/jickchan/saveimagedata (#17)
    * feat: image dto 생성
    
    * feat: imageMapper 생성
    
    * feat: java business service 단 생성
    
    * feat: kotlin db에 메타데이터 저장 로직 추가
    
    * feat: kotlin db에 메타데이터 저장 테스트 api 생성
    
    * refactor: originFileName Hashing
    
    * chore: update git submodule
    
    ---------
    
    Co-authored-by: yeongun <gjduddns@gmail.com>

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/image/service/ImageService.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/image/service/ImageService.kt
@@ -2,28 +2,55 @@ package com.doonutmate.image.service
 
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.ObjectMetadata
+import com.doonutmate.doonut.image.model.Image
+import com.doonutmate.doonut.image.service.ImageBusinessService
+import com.doonutmate.image.ImageMeta
+import com.doonutmate.image.ImageMetaSupporter
 import com.doonutmate.image.controller.dto.ImageUploadResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.util.UUID
 
 @Service
 class ImageService(
     private val amazonS3: AmazonS3,
+    private val imageBusinessService: ImageBusinessService,
 ) {
 
     @Value("\${cloud.aws.s3.bucket}")
     private val bucket: String? = null
 
     fun saveFile(multipartFile: MultipartFile): ImageUploadResponse {
-        val originalFilename = multipartFile.originalFilename
-        val metadata = ObjectMetadata()
-        metadata.setContentLength(multipartFile.size)
-        metadata.setContentType(multipartFile.contentType)
+        val randomKey = UUID.randomUUID().toString()
+        val imageUrl = saveFileToS3(multipartFile, randomKey)
 
-        amazonS3.putObject(bucket, originalFilename, multipartFile.inputStream, metadata)
-        val imageUrl = amazonS3.getUrl(bucket, originalFilename).toString()
+        saveFileToDb(multipartFile, randomKey, imageUrl)
 
         return ImageUploadResponse(imageUrl)
+    }
+
+    private fun saveFileToS3(multipartFile: MultipartFile, key: String): String {
+        val metadata = ObjectMetadata()
+        metadata.contentLength = multipartFile.size
+        metadata.contentType = multipartFile.contentType
+
+        amazonS3.putObject(bucket, key, multipartFile.inputStream, metadata)
+
+        return amazonS3.getUrl(bucket, key).toString()
+    }
+
+    private fun saveFileToDb(multipartFile: MultipartFile, key: String, imageUrl: String): Long {
+        val imageMeta: ImageMeta = ImageMetaSupporter.extract(multipartFile)
+        val newImage = Image.builder()
+            .imageKey(key)
+            .oriImageName(multipartFile.originalFilename)
+            .imageHostUrl(imageUrl)
+            .height(imageMeta.height)
+            .width(imageMeta.widht)
+            .capacity(imageMeta.capacity)
+            .deleted(false)
+            .build()
+        return imageBusinessService.create(newImage)
     }
 }

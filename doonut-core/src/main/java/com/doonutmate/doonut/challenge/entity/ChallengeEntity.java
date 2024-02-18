a6210a6b3d511fced75b928a2996ee5b41eeb272
jickDo <pocard0117@naver.com>
2024-02-18 23:07:21 +0900

    feature: challenge entity create

---
+++ doonut-core/src/main/java/com/doonutmate/doonut/challenge/entity/ChallengeEntity.java
@@ -0,0 +1,31 @@
+package com.doonutmate.doonut.challenge.entity;
+
+import com.doonutmate.doonut.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "challenge")
+public class ChallengeEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    Long id;
+
+    @Column
+    Long memberId;
+
+    @Column
+    String imageUrl;
+
+    @Column
+    boolean deleted;
+}

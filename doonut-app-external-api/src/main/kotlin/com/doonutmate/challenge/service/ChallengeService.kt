c82d64dcd17f8cc4979ac887c32620f006150c5a
jickDo <pocard0117@naver.com>
2024-02-19 12:10:11 +0900

    feature: external-api packege challenge service create a structure

---
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/challenge/service/ChallengeService.kt
@@ -0,0 +1,11 @@
+package com.doonutmate.challenge.service
+
+import com.doonutmate.doonut.challenge.service.ChallengeBusinessServicee
+import org.springframework.stereotype.Service
+
+@Service
+class ChallengeService(
+    private val service: ChallengeBusinessServicee,
+){
+
+}

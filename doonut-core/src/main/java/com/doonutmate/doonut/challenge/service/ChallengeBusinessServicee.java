abb66627c6f059f8707080b1528333efe3ed2808
jickDo <pocard0117@naver.com>
2024-02-20 04:42:44 +0900

    chore: imgaeRepo 등록

--- doonut-core/src/main/java/com/doonutmate/doonut/challenge/service/ChallengeBusinessServicee.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/challenge/service/ChallengeBusinessServicee.java
@@ -3,31 +3,38 @@ package com.doonutmate.doonut.challenge.service;
 import com.doonutmate.doonut.challenge.mapper.ChallengeMapper;
 import com.doonutmate.doonut.challenge.model.Challenge;
 import com.doonutmate.doonut.challenge.repository.ChallengeRepository;
+import com.doonutmate.doonut.image.mapper.ImageMapper;
 import com.doonutmate.doonut.image.model.Image;
+import com.doonutmate.doonut.image.repository.ImageRepository;
+import com.doonutmate.doonut.image.service.ImageBusinessService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class ChallengeBusinessServicee {
-    private final ChallengeRepository repository;
+    private final ChallengeRepository challengeRepository;
+    private final ImageRepository imageRepository;
     private final ChallengeMapper mapper;
 
     @Transactional
     public Long create(Challenge challenge) {
         var newEntity = mapper.toEntity(challenge);
 
-        var savedEntity = repository.save(newEntity);
+        var savedEntity = challengeRepository.save(newEntity);
 
         return savedEntity.getId();
     }
 
     public Challenge get(Long id) {
-        return repository.findById(id)
+        return challengeRepository.findById(id)
                 .map(mapper::toModel)
                 .orElse(null);
     }
+
 
 }

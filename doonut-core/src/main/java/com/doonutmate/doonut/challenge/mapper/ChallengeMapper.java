39e55c5bd842d0f81a72093b4cf3fba000de18e3
jickDo <pocard0117@naver.com>
2024-02-19 12:01:46 +0900

    feature: challenge mapper implement

--- doonut-core/src/main/java/com/doonutmate/doonut/challenge/mapper/ChallengeMapper.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/challenge/mapper/ChallengeMapper.java
@@ -1,4 +1,14 @@
 package com.doonutmate.doonut.challenge.mapper;
 
+import com.doonutmate.doonut.challenge.entity.ChallengeEntity;
+import com.doonutmate.doonut.challenge.model.Challenge;
+import com.doonutmate.doonut.image.entity.ImageEntity;
+import com.doonutmate.doonut.image.model.Image;
+import org.mapstruct.Mapper;
+
+@Mapper
 public interface ChallengeMapper {
+    ChallengeEntity toEntity(Challenge model);
+
+    Challenge toModel(ChallengeEntity entity);
 }

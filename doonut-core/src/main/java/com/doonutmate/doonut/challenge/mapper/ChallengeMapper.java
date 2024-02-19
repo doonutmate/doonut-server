b1bffb456ce53c430a645c628c69f21ba962e14a
jickDo <pocard0117@naver.com>
2024-02-20 04:44:14 +0900

    chore: ChallengeMapper 추가

--- doonut-core/src/main/java/com/doonutmate/doonut/challenge/mapper/ChallengeMapper.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/challenge/mapper/ChallengeMapper.java
@@ -5,10 +5,28 @@ import com.doonutmate.doonut.challenge.model.Challenge;
 import com.doonutmate.doonut.image.entity.ImageEntity;
 import com.doonutmate.doonut.image.model.Image;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 @Mapper
 public interface ChallengeMapper {
+
+    @Mapping(target = "days", ignore = true)
     ChallengeEntity toEntity(Challenge model);
 
+    @Mapping(target = "days", ignore = true)
     Challenge toModel(ChallengeEntity entity);
+
+    @Mapping(target = "imageUrl", source = "imageKey")
+    @Mapping(target = "days", ignore = true)
+    Challenge toChallenge(Image entity);
+
+    @Mapping(target = "imageUrl", source = "imageKey")
+    @Mapping(target = "days", ignore = true)
+    List<Challenge> toChallengeList(List<Image> otherModelList);
+
+
 }

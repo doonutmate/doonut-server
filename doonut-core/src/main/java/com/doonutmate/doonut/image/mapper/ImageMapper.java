ef810784fd7b65e891c267b1d6fa24f1fae2d898
jickDo <pocard0117@naver.com>
2024-02-20 04:46:43 +0900

    chore: image멤버 toModelList생성

--- doonut-core/src/main/java/com/doonutmate/doonut/image/mapper/ImageMapper.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/image/mapper/ImageMapper.java
@@ -1,9 +1,12 @@
 package com.doonutmate.doonut.image.mapper;
 
+import com.doonutmate.doonut.challenge.model.Challenge;
 import com.doonutmate.doonut.image.entity.ImageEntity;
 import com.doonutmate.doonut.image.model.Image;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper
 public interface ImageMapper {
@@ -11,5 +14,7 @@ public interface ImageMapper {
     ImageEntity toEntity(Image model);
 
     Image toModel(ImageEntity entity);
+
+    List<Image> toModelList(List<ImageEntity> model);
 
 }

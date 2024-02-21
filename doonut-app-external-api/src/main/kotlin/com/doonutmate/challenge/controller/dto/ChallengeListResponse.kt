1946e37eaf91265390561b3dd8cb6815bd90a2ae
jickDo <pocard0117@naver.com>
2024-02-21 16:00:57 +0900

    refactor: day값을 String으로 변경

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/challenge/controller/dto/ChallengeListResponse.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/challenge/controller/dto/ChallengeListResponse.kt
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(title = "Challenge 목록 조회 응답")
 data class ChallengeListResponse(
     @Schema(title = "일", example = "31")
-    var day: Int,
+    var day: String,
 
     @Schema(title = "이미지 url")
     val imageUrl: String,

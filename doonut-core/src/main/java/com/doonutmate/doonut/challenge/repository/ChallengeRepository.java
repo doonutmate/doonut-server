15423d6951760e9f4b041bac313810ed049ca4cb
jickDo <pocard0117@naver.com>
2024-02-21 14:19:55 +0900

    feature: ChallengeRepository에 딜리트와 날짜를 기반으로 jpa 함수구현

--- doonut-core/src/main/java/com/doonutmate/doonut/challenge/repository/ChallengeRepository.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/challenge/repository/ChallengeRepository.java
@@ -1,7 +1,28 @@
 package com.doonutmate.doonut.challenge.repository;
 
 import com.doonutmate.doonut.challenge.entity.ChallengeEntity;
+import com.doonutmate.doonut.image.entity.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ChallengeRepository extends JpaRepository<ChallengeEntity, Long> {
+
+
+    @Query("""
+                SELECT challenge
+                FROM ChallengeEntity challenge
+                WHERE challenge.deleted = false 
+                AND challenge.memberId = :memberId
+                AND FUNCTION('YEAR', challenge.createdAt) = :year
+                AND FUNCTION('MONTH', challenge.createdAt) = :month
+            """)
+    Optional<List<ChallengeEntity>> findAllByMemberIdAndDate(
+            @Param("memberId") String memberId,
+            @Param("year") int year,
+            @Param("month") int month
+    );
 }

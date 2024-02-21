4a1861e4ea78f6ca6cf831e499151c9650f03932
jickDo <pocard0117@naver.com>
2024-02-21 16:31:34 +0900

    chore: 매핑 설정 추가

--- doonut-core/src/main/java/com/doonutmate/doonut/challenge/mapper/ChallengeMapper.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/challenge/mapper/ChallengeMapper.java
@@ -2,20 +2,17 @@ package com.doonutmate.doonut.challenge.mapper;
 
 import com.doonutmate.doonut.challenge.entity.ChallengeEntity;
 import com.doonutmate.doonut.challenge.model.Challenge;
-import com.doonutmate.doonut.image.entity.ImageEntity;
-import com.doonutmate.doonut.image.model.Image;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.OptionalInt;
 
 @Mapper
 public interface ChallengeMapper {
 
     ChallengeEntity toEntity(Challenge model);
 
+    @Mapping(target = "created_at", source = "createdAt")
     Challenge toModel(ChallengeEntity entity);
 
     List<Challenge> toChallengeList(List<ChallengeEntity> otherModelList);

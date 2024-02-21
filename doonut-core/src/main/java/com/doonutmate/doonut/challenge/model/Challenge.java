54197396bdaecdfa8e8b0c6ef5caaa091d62564c
jickDo <pocard0117@naver.com>
2024-02-21 16:32:51 +0900

    chore: Challenge 에서 생성시간을 담도록 변경

--- doonut-core/src/main/java/com/doonutmate/doonut/challenge/model/Challenge.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/challenge/model/Challenge.java
@@ -2,12 +2,16 @@ package com.doonutmate.doonut.challenge.model;
 
 import lombok.Builder;
 
+import java.time.Instant;
+
 @Builder
 public record Challenge(
         Long id,
         String memberId,
         String imageUrl,
         ChallengeType type,
+
+        Instant created_at,
         boolean deleted
 ) {
 }

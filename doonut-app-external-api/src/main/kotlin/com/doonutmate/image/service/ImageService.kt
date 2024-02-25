e8afdf17172c84618e0a0acc268dbcb2bf99bfb8
yeongun <gjduddns@gmail.com>
2024-02-25 21:03:49 +0900

    refactor: ImageFacadeService 생성

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/image/service/ImageService.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/image/service/ImageService.kt
@@ -2,12 +2,6 @@ package com.doonutmate.image.service
 
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.ObjectMetadata
-import com.doonutmate.doonut.challenge.model.Challenge
-import com.doonutmate.doonut.challenge.service.ChallengeBusinessServicee
-import com.doonutmate.doonut.image.model.Image
-import com.doonutmate.doonut.image.service.ImageBusinessService
-import com.doonutmate.image.ImageMeta
-import com.doonutmate.image.ImageMetaSupporter
 import com.doonutmate.image.controller.dto.ImageUploadResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -17,23 +11,19 @@ import java.util.UUID
 @Service
 class ImageService(
     private val amazonS3: AmazonS3,
-    private val imageBusinessService: ImageBusinessService,
-    private val challengeBusinessService: ChallengeBusinessServicee,
+    private val imageFacadeService: ImageFacadeService,
 ) {
 
     @Value("\${cloud.aws.s3.bucket}")
     private val bucket: String? = null
 
-    @Value("\${cloud.aws.cloudfront.prefix}")
-    private val imageHostUrlPrefix: String? = null
-
     fun saveFile(multipartFile: MultipartFile, memberId: Long): ImageUploadResponse {
         val randomKey = UUID.randomUUID().toString()
 
         saveFileToS3(multipartFile, randomKey)
-        saveFileToDb(multipartFile, randomKey, memberId)
+        val imageUrl = saveFileToDb(multipartFile, randomKey, memberId)
 
-        return ImageUploadResponse(getImageUrl(randomKey))
+        return ImageUploadResponse(imageUrl)
     }
 
     private fun saveFileToS3(multipartFile: MultipartFile, key: String) {
@@ -46,36 +36,7 @@ class ImageService(
         amazonS3.getUrl(bucket, key).toString()
     }
 
-    private fun saveFileToDb(multipartFile: MultipartFile, key: String, memberId: Long) {
-        val imageUrl = saveImage(multipartFile, key, memberId)
-        saveChallenge(memberId, imageUrl)
+    private fun saveFileToDb(multipartFile: MultipartFile, key: String, memberId: Long): String {
+        return imageFacadeService.save(multipartFile, key, memberId)
     }
-
-    private fun saveImage(multipartFile: MultipartFile, key: String, memberId: Long): String {
-        val imageMeta: ImageMeta = ImageMetaSupporter.extract(multipartFile)
-        val imageUrl = getImageUrl(key)
-        val newImage = Image.builder()
-            .imageKey(key)
-            .oriImageName(multipartFile.originalFilename)
-            .imageHostUrl(imageUrl)
-            .height(imageMeta.height)
-            .width(imageMeta.widht)
-            .capacity(imageMeta.capacity)
-            .memberId(memberId)
-            .deleted(false)
-            .build()
-        imageBusinessService.create(newImage)
-        return imageUrl
-    }
-
-    private fun saveChallenge(memberId: Long, imageUrl: String) {
-        val newChallenge = Challenge.builder()
-            .memberId(memberId)
-            .imageUrl(imageUrl)
-            .deleted(false)
-            .build()
-        challengeBusinessService.create(newChallenge)
-    }
-
-    private fun getImageUrl(key: String) = imageHostUrlPrefix + key
 }

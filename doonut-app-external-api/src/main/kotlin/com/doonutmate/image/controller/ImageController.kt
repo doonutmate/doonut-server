ebc606660e71a073f9181e6dda4f4942640cb5db
yeongun <gjduddns@gmail.com>
2024-02-25 20:29:14 +0900

    chore: ChallengeController 함수명변경

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/image/controller/ImageController.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/image/controller/ImageController.kt
@@ -1,18 +1,12 @@
 package com.doonutmate.image.controller
 
-import com.doonutmate.challenge.controller.dto.ChallengeListRequest
-import com.doonutmate.challenge.controller.dto.ChallengeListResponse
-import com.doonutmate.doonut.image.model.Image
 import com.doonutmate.image.controller.dto.ImageUploadResponse
 import com.doonutmate.image.service.ImageService
 import com.doonutmate.oauth.configuration.Authorization
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
-import jakarta.validation.Valid
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
@@ -31,25 +25,10 @@ class ImageController(
         consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE],
     )
-    fun login(
+    fun imageUpload(
         @RequestPart("multipartFile") multipartFile: MultipartFile,
-        @Authorization
-        @Parameter(hidden = true)
-        userId: Long,
+        @Authorization @Parameter(hidden = true) userId: Long,
     ): ImageUploadResponse {
         return imageService.saveFile(multipartFile, userId)
     }
-
-//    @Operation(summary = "특정 달에 해당하는 챌린지 목록 조회", description = "특정 연월을 입력으로 받으면 챌린지 목록을 반환한다.")
-//    @GetMapping
-//    fun getList(
-//        @Authorization
-//        @Parameter(hidden = true)
-//        memberId: String,
-//
-//        @Valid @ModelAttribute
-//        req: ChallengeListRequest,
-//    ): List<Image> {
-//        return imageService.getImage(memberId, req)
-//    }
 }

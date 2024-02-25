6cdbee02d24885e57394cd85feec0f1ea663916d
yeongun <gjduddns@gmail.com>
2024-02-25 20:43:20 +0900

    refactor: 리사이징용 API 제거

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/challenge/controller/ChallengeController.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/challenge/controller/ChallengeController.kt
@@ -3,22 +3,15 @@ package com.doonutmate.challenge.controller
 import com.doonutmate.challenge.controller.dto.ChallengeListRequest
 import com.doonutmate.challenge.controller.dto.ChallengeListResponse
 import com.doonutmate.challenge.service.ChallengeService
-import com.doonutmate.doonut.challenge.model.ChallengeType
-import com.doonutmate.image.controller.dto.ImageUploadResponse
 import com.doonutmate.oauth.configuration.Authorization
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @Tag(name = "Challenge", description = "챌린지 API")
@@ -38,21 +31,5 @@ class ChallengeController(
         req: ChallengeListRequest,
     ): List<ChallengeListResponse> {
         return service.getChallengeList(memberId, req)
-    }
-
-    @Operation(summary = "리사이징된 사진을 s3와 db에 저장", description = "")
-    @PostMapping(
-        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE],
-    )
-    fun saveResizingChallengeImage(
-        @Authorization
-        @Parameter(hidden = true)
-        memberId: Long,
-
-        @RequestPart("multipartFile") multipartFile: MultipartFile,
-        @RequestParam type: ChallengeType,
-    ): ImageUploadResponse {
-        return service.saveResizingImage(multipartFile, type, memberId)
     }
 }

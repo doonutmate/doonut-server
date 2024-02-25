430d0af9f41c82a4ad711d20cdb984939573121e
yeongun <gjduddns@gmail.com>
2024-02-25 23:06:22 +0900

    refactor: 사용하지 않는 메서드 제거

--- doonut-core/src/main/java/com/doonutmate/doonut/image/repository/ImageRepository.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/image/repository/ImageRepository.java
@@ -1,26 +1,7 @@
 package com.doonutmate.doonut.image.repository;
 
 import com.doonutmate.doonut.image.entity.ImageEntity;
-import com.doonutmate.doonut.image.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
-    @Query("""
-                SELECT image
-                FROM ImageEntity image
-                WHERE image.deleted = false 
-                AND image.memberId = :memberId
-                AND FUNCTION('YEAR', image.createdAt) = :year
-                AND FUNCTION('MONTH', image.createdAt) = :month
-            """)
-    Optional<List<ImageEntity>> findAllByMemberIdAndDate(
-            @Param("memberId") String memberId,
-            @Param("year") int year,
-            @Param("month") int month
-    );
 }

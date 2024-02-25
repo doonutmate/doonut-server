7ceb4411a550280ae3ee8cb152ecc89a92070f59
yeongun <gjduddns@gmail.com>
2024-02-25 23:13:38 +0900

    refactor: Challenge ddl 들여쓰기 수정

--- doonut-core/src/main/resources/db/migration/V20240218225919__challenge_ddl.sql
+++ doonut-core/src/main/resources/db/migration/V20240218225919__challenge_ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE challenge
 (
-    id             BIGINT AUTO_INCREMENT NOT NULL COMMENT 'ID',
-    member_id      BIGINT NULL COMMENT '멤버 ID',
+    id             BIGINT AUTO_INCREMENT                NOT NULL COMMENT 'ID',
+    member_id      BIGINT                               NOT NULL COMMENT '멤버 ID',
     image_url      VARCHAR(500)                         NOT NULL COMMENT '이미지 url',
     deleted        BIT       DEFAULT 0                  NOT NULL COMMENT '삭제 여부',
     created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL COMMENT '생성 시간',

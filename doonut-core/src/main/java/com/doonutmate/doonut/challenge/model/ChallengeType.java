99a46e56f8cc88a91ee644d23101e7e6d5094e65
yeongun <gjduddns@gmail.com>
2024-02-25 22:45:21 +0900

    feat: 챌린지 이미지 url을 받으면 각각 디폴트, 썸네일 url을 만들어주는 메서드 추가

--- doonut-core/src/main/java/com/doonutmate/doonut/challenge/model/ChallengeType.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/challenge/model/ChallengeType.java
@@ -8,11 +8,22 @@ public enum ChallengeType {
     THUMBNAIL(46, 46)
     ;
 
+    public static final String QUERY_STRING_FORMAT = "?w=%s&h=%s";
     private final int width;
     private final int height;
 
     ChallengeType(int width, int height) {
         this.width = width;
         this.height = height;
+    }
+
+    public static String getDefaultUrl(String url) {
+        var queryString = String.format(QUERY_STRING_FORMAT, ChallengeType.DEFAULT.getWidth(), ChallengeType.DEFAULT.getHeight());
+        return url + queryString;
+    }
+
+    public static String getThumbNailUrl(String url) {
+        var queryString = String.format(QUERY_STRING_FORMAT, ChallengeType.THUMBNAIL.getWidth(), ChallengeType.THUMBNAIL.getHeight());
+        return url + queryString;
     }
 }

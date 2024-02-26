f5cf9c7641ce68459af0ab63e674a07519f15a8c
yeongun <gjduddns@gmail.com>
2024-02-26 17:07:13 +0900

    refactor: S3 저장 시 Active profile, 날짜 순으로 경로가 구분되도록 변경

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/image/service/ImageAppService.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/image/service/ImageAppService.kt
@@ -2,11 +2,17 @@ package com.doonutmate.image.service
 
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.ObjectMetadata
+import com.doonutmate.image.ImageMetaSupporter
 import com.doonutmate.image.controller.dto.ImageUploadResponse
+import com.doonutmate.util.CommonDateUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.time.Instant
 import java.util.UUID
+
+private const val FOLDER_DELIMITER = "/"
+private const val FILE_EXTENSION_DELIMITER = "."
 
 @Service
 class ImageAppService(
@@ -17,8 +23,11 @@ class ImageAppService(
     @Value("\${cloud.aws.s3.bucket}")
     private val bucket: String? = null
 
+    @Value("\${spring.profiles.active:local}")
+    private val activeProfile: String? = null
+
     fun saveFile(multipartFile: MultipartFile, memberId: Long): ImageUploadResponse {
-        val randomKey = UUID.randomUUID().toString()
+        val randomKey = resolveObjectKey(multipartFile)
 
         saveFileToS3(multipartFile, randomKey)
         // TODO 기존 이미지 삭제 로직 추가 필요
@@ -39,5 +48,13 @@ class ImageAppService(
 
     private fun saveFileToDb(multipartFile: MultipartFile, key: String, memberId: Long): String {
         return imageFacadeService.save(multipartFile, key, memberId)
+    }
+
+    private fun resolveObjectKey(multipartFile: MultipartFile): String {
+        val randomUUID = UUID.randomUUID().toString()
+        val extension = ImageMetaSupporter.getExtension(multipartFile.originalFilename)
+        val date = CommonDateUtils.getYearMonthDay(Instant.now())
+
+        return activeProfile + FOLDER_DELIMITER + date + FOLDER_DELIMITER + randomUUID + FILE_EXTENSION_DELIMITER + extension
     }
 }

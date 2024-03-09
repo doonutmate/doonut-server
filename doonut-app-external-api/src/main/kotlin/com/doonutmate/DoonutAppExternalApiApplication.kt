c2b7e5025179923d5bf93f1d18d63f4d9c75b1ef
Yeongun Heo <52141636+yeongunheo@users.noreply.github.com>
2024-03-09 09:39:18 +0900

    chore: Swagger에서 CORS가 발생하는 에러 수정 (#31)

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/DoonutAppExternalApiApplication.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/DoonutAppExternalApiApplication.kt
@@ -1,8 +1,11 @@
 package com.doonutmate
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(servers = [Server(url = "/", description = "Default Server URL")])
 @SpringBootApplication
 class DoonutAppExternalApiApplication
 

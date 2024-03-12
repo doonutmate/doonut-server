6984c3e8560a3d15e5e36c1c06f95da53d534af0
jickDo <pocard0117@naver.com>
2024-03-12 12:56:08 +0900

    chore: 주소 상수값추가

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/apple/constants/AppleOauthConstants.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/apple/constants/AppleOauthConstants.kt
@@ -3,4 +3,6 @@ package com.doonutmate.oauth.apple.constants
 object AppleOauthConstants {
     const val APPLE_URL = "https://appleid.apple.com"
     const val GET_PUBLIC_KEYS = "/auth/keys"
+    const val CREATE_TOKEN = "/auth/token"
+    const val REVOKE_TOKEN = "/auth/revoke"
 }

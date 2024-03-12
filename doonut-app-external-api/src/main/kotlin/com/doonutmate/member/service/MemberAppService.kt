3749d629ce28b750995d3554f98e6ec5edaa9af4
jickDo <pocard0117@naver.com>
2024-03-12 12:55:54 +0900

    feature: 메서드 오버로딩 애플삭제 로직추가

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/member/service/MemberAppService.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/member/service/MemberAppService.kt
@@ -1,16 +1,25 @@
 package com.doonutmate.member.service
 
 import com.doonutmate.doonut.member.service.MemberBusinessService
+import com.doonutmate.oauth.apple.dto.AppleTokenResponse
+import com.doonutmate.oauth.apple.service.AppleOauthProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class MemberAppService(
     @Autowired var memberBusinessService: MemberBusinessService,
+    private val appleOauthProvider: AppleOauthProvider,
 ) {
     fun delete(memberId: Long) {
         memberBusinessService.delete(memberId)
-
         // TODO 애플로 로그인한 멤버면 APPLE 서버로 탈퇴 API를 날리도록 로직 추가
+    }
+
+    fun delete(memberId: Long, code: String) {
+        val response: AppleTokenResponse = appleOauthProvider.createAuthToken(code)
+        val accessToken = appleOauthProvider.createAuthToken(code).accessToken
+        appleOauthProvider.revokeAccessToken(accessToken)
+        memberBusinessService.delete(memberId)
     }
 }

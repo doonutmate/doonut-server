fd47754d8f3098cfeeb03cfc7725161bd7a2f1c1
jickDo <pocard0117@naver.com>
2024-03-13 23:48:00 +0900

    chore: nullable

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/apple/dto/AppleOauthRequest.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/oauth/apple/dto/AppleOauthRequest.kt
@@ -1,7 +1,7 @@
 package com.doonutmate.oauth.apple.dto
 
 data class AppleOauthRequest(
-    val oauthId: String,
-    val name: String,
-    val email: String
+    val oauthId: String?,
+    val name: String?,
+    val email: String?
 )

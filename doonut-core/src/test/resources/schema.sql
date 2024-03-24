f58fbe3c37b9fa3c3aa0d872dcb60a8afd22cb86
yeongun <gjduddns@gmail.com>
2024-03-24 17:43:52 +0900

    chore: 회원 탈퇴 사유 sql문 추가

--- doonut-core/src/test/resources/schema.sql
+++ doonut-core/src/test/resources/schema.sql
@@ -55,3 +55,13 @@ CREATE TABLE IF NOT EXISTS challenge
     updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL COMMENT '수정 시간',
     primary key (id)
 );
+
+CREATE TABLE IF NOT EXISTS member_delete_reason
+(
+    id         BIGINT AUTO_INCREMENT NOT NULL COMMENT 'ID',
+    member_id  BIGINT                              NOT NULL COMMENT '멤버 ID',
+    reason     VARCHAR(500)                        NOT NULL COMMENT '탈퇴 이유',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성 시간',
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '수정 시간',
+    PRIMARY KEY (id)
+);

7df6ebf53c69aafc71b5bc1c9e0462cbc61acb21
yeongun <gjduddns@gmail.com>
2024-03-24 14:42:57 +0900

    test: @SpringBootTest에 @Transactional 테스트 어노테이션 추가

--- doonut-core/src/test/java/com/doonutmate/doonut/member/repository/MemberRepositoryTest.java
+++ doonut-core/src/test/java/com/doonutmate/doonut/member/repository/MemberRepositoryTest.java
@@ -7,9 +7,11 @@ import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class MemberRepositoryTest {
 

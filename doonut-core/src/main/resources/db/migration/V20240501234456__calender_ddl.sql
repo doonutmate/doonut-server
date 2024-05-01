cbc19d359098f2bb8052222bd81b2e777536225e
jickDo <pocard0117@naver.com>
2024-05-02 00:34:12 +0900

    feature: 캘린더 플라이웨이 적용

---
+++ doonut-core/src/main/resources/db/migration/V20240501234456__calender_ddl.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS calender
+(
+    id                BIGINT AUTO_INCREMENT               NOT NULL COMMENT 'ID',
+    member_id         BIGINT                              NOT NULL COMMENT '멤버 ID',
+    calender_name     VARCHAR(100)                        NOT NULL COMMENT '캘린더 명',
+    total_count       INT                                 NOT NULL COMMENT '총 기록 횟수',
+    first_upload_date  TIMESTAMP                               NULL COMMENT '최초 업로드 날짜',
+    last_upload_date  TIMESTAMP                               NULL COMMENT '마지막 업로드 날짜',
+    deleted           BIT       DEFAULT 0                 NOT NULL COMMENT '삭제 여부',
+    created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성 시간',
+    updated_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '수정 시간',
+    PRIMARY KEY (id)
+);

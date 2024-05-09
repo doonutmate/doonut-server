cbd18ac7c559fcabf680b80918bb23ee951282fa
jickDo <pocard0117@naver.com>
2024-05-09 19:46:56 +0900

    feat: 비즈니스 서비스 단 & 레포지토리 설정

--- doonut-core/src/main/java/com/doonutmate/doonut/calendar/repository/CalendarRepository.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/calendar/repository/CalendarRepository.java
@@ -1,7 +1,19 @@
 package com.doonutmate.doonut.calendar.repository;
 
 import com.doonutmate.doonut.calendar.entity.CalendarEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CalendarRepository extends JpaRepository<CalendarEntity, Long> {
+    @Query("SELECT c FROM CalendarEntity c WHERE c.deleted = false")
+    List<CalendarEntity> findFirstByDeletedFalseOrderByIdAsc(Pageable pageable);
+
+    @Query("SELECT count(c) FROM CalendarEntity c WHERE c.deleted = false")
+    long countByDeletedFalse();
+
+    @Query("SELECT c FROM CalendarEntity c WHERE c.deleted = false AND c.id > :cursor ORDER BY c.id ASC")
+    List<CalendarEntity> findByCursor(Long cursor, Pageable pageable);
 }
--- doonut-core/src/main/java/com/doonutmate/doonut/calendar/service/CalendarBusinessService.java
+++ doonut-core/src/main/java/com/doonutmate/doonut/calendar/service/CalendarBusinessService.java
@@ -1,11 +1,19 @@
 package com.doonutmate.doonut.calendar.service;
 
+import com.doonutmate.doonut.calendar.entity.CalendarEntity;
 import com.doonutmate.doonut.calendar.repository.CalendarRepository;
 import com.doonutmate.doonut.calendar.mapper.CalendarMapper;
 import com.doonutmate.doonut.calendar.model.Calendar;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -13,6 +21,39 @@ import org.springframework.transaction.annotation.Transactional;
 public class CalendarBusinessService {
     private final CalendarRepository repository;
     private final CalendarMapper mapper;
+
+
+    private final static int FIRST_INDEX = 0;
+
+    public List<Calendar> findByCursor(Long cursor, Pageable pageable) {
+        List<CalendarEntity> calendarEntityList = repository.findByCursor(cursor, pageable);
+
+        return calendarEntityList.stream()
+                .map(calendarEntity -> get(calendarEntity.getId()))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    public Long getInitialCursor() {
+        List<CalendarEntity> calendars = findFirstCalendar(FIRST_INDEX);
+        return extractFirstItemId(calendars);
+    }
+
+    public Long getRandomCursor() {
+        long count = repository.countByDeletedFalse();
+        int randomIndex = new Random().nextInt((int) count);
+        List<CalendarEntity> calendars =findFirstCalendar(randomIndex);
+        return extractFirstItemId(calendars);
+    }
+
+    private List<CalendarEntity> findFirstCalendar(int index) {
+        return repository.findFirstByDeletedFalseOrderByIdAsc(PageRequest.of(index, 1));
+    }
+
+
+    private Long extractFirstItemId(List<CalendarEntity> calendars) {
+        return calendars.isEmpty() ? null : calendars.get(0).getId();
+    }
 
     @Transactional
     public Long create(Calendar calendar) {

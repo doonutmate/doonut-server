96009afb33c5523f294490a6339e4f9fb207f415
jickDo <pocard0117@naver.com>
2024-05-20 16:01:56 +0900

    feat: 도메인 영역내 설문 신고기능에 해당하는 영역 설계

---
+++ doonut-core/src/main/java/com/doonutmate/doonut/calendar/entity/CalendarReportReasonEntity.java
@@ -0,0 +1,44 @@
+package com.doonutmate.doonut.calendar.entity;
+
+import com.doonutmate.doonut.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "calendar_report_reason")
+public class CalendarReportReasonEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    Long id;
+
+    @Column(nullable = false)
+    Long reporterId;
+
+    @Column(nullable = false)
+    Long calendarId;
+
+    @Column(nullable = false, length = 200)
+    @Size(min = 10, max = 200)
+    String reason;
+}

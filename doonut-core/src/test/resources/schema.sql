ee0cbd84251753143c12dd1e85d31155d953c4ad
jickDo <pocard0117@naver.com>
2024-05-24 22:20:49 +0900

    chore: 테스트 디비 sql 추가

--- doonut-core/src/test/resources/schema.sql
+++ doonut-core/src/test/resources/schema.sql
@@ -36,10 +36,11 @@ CREATE TABLE IF NOT EXISTS member
 
 CREATE TABLE IF NOT EXISTS profile_image
 (
-    id         BIGINT AUTO_INCREMENT NOT NULL COMMENT 'ID',
-    member_id  BIGINT NULL COMMENT '멤버 ID',
-    image_type VARCHAR(100)                         NOT NULL COMMENT '이미지 타입',
-    image_url  VARCHAR(500)                         NOT NULL COMMENT '이미지 url',
+    id         BIGINT AUTO_INCREMENT               NOT NULL COMMENT 'ID',
+    member_id  BIGINT                              NULL COMMENT '멤버 ID',
+    image_type VARCHAR(100)                        NOT NULL COMMENT '이미지 타입',
+    image_url  VARCHAR(500)                        NOT NULL COMMENT '이미지 url',
+    deleted    BIT       DEFAULT 0                 NOT NULL COMMENT '삭제 여부',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성 시간',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '수정 시간',
     PRIMARY KEY (id)
@@ -63,5 +64,30 @@ CREATE TABLE IF NOT EXISTS member_delete_reason
     reason     VARCHAR(500)                        NOT NULL COMMENT '탈퇴 이유',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성 시간',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '수정 시간',
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS calendar
+(
+    id                BIGINT AUTO_INCREMENT               NOT NULL COMMENT 'ID',
+    member_id         BIGINT                              NOT NULL COMMENT '멤버 ID',
+    calendar_name     VARCHAR(100)                        NOT NULL COMMENT '캘린더 명',
+    total_count       INT       DEFAULT 0                 NOT NULL COMMENT '총 기록 횟수',
+    first_uploaded_at TIMESTAMP                           NULL COMMENT '최초 업로드 날짜',
+    last_uploaded_at  TIMESTAMP                           NULL COMMENT '마지막 업로드 날짜',
+    deleted           BIT       DEFAULT 0                 NOT NULL COMMENT '삭제 여부',
+    created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성 시간',
+    updated_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '수정 시간',
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS calendar_report_reason
+(
+    id          BIGINT AUTO_INCREMENT               NOT NULL COMMENT 'ID',
+    reporter_id BIGINT                              NOT NULL COMMENT '신고자 ID',
+    calendar_id BIGINT                              NOT NULL COMMENT '신고된 캘린더 ID',
+    reason      VARCHAR(200)                        NOT NULL COMMENT '신고 이유',
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성 시간',
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '수정 시간',
     PRIMARY KEY (id)
 );

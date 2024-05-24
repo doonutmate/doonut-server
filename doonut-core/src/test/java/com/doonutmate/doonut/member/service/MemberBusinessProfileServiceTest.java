b3bdd00d5d1fa37096f07d64ff9ae86bdc08a0d5
jickDo <pocard0117@naver.com>
2024-05-24 22:22:27 +0900

    chore: 테스트생성

---
+++ doonut-core/src/test/java/com/doonutmate/doonut/member/service/MemberBusinessProfileServiceTest.java
@@ -0,0 +1,87 @@
+package com.doonutmate.doonut.member.service;
+
+import com.doonutmate.doonut.member.entity.MemberEntity;
+import com.doonutmate.doonut.member.entity.OauthInfo;
+import com.doonutmate.doonut.member.entity.ProfileImageEntity;
+import com.doonutmate.doonut.member.model.ImageType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@SpringBootTest
+public class MemberBusinessProfileServiceTest {
+
+    private static final ImageType REPRESENTATIVE = ImageType.REPRESENTATIVE;
+    private static final String EXPECTED_URL = "expected url";
+    private static final String EXPECTED_NAME = "expected name";
+
+
+    @DisplayName("회원은 이름, 프로필을 수정할수 있다")
+    @Test
+    void updateProfile() {
+        //given
+        MemberEntity member = generateMember();
+        ProfileImageEntity profileImage = generateProfileEntity(3L, EXPECTED_URL, REPRESENTATIVE, member, false);
+        List<ProfileImageEntity> actualList = generateProfileList();
+
+        //when
+        actualList.add(profileImage);
+        member.updateNameOrProfileImage(EXPECTED_NAME, actualList);
+
+        //then
+        Assertions.assertEquals(EXPECTED_URL, member.getProfileImages().getLast().getImageUrl());
+        Assertions.assertEquals(EXPECTED_NAME, member.getName());
+    }
+
+    @DisplayName("deleted== false && ImageType.REPRESENTATIVE 프로필 이미지가 모두 소프트 딜리트 되어야 한다.")
+    @Test
+    void deleteRepresentativeImage() {
+        // given
+        List<ProfileImageEntity> actualList = generateProfileList();
+
+        // when
+        actualList.forEach(profileImage -> {
+            if (profileImage.getImageType().equals(ImageType.REPRESENTATIVE) && !profileImage.isDeleted()) {
+                profileImage.delete();
+            }
+        });
+
+        // then
+        boolean allDeleted = actualList.stream()
+                .filter(profileImage -> profileImage.getImageType().equals(ImageType.REPRESENTATIVE))
+                .allMatch(ProfileImageEntity::isDeleted);
+
+        Assertions.assertTrue(allDeleted, "삭제되지 않은 이미지가 있습니다");
+    }
+
+    private List<ProfileImageEntity> generateProfileList() {
+        List<ProfileImageEntity> profileList = new ArrayList<>();
+        profileList.add(generateProfileEntity(1L, EXPECTED_URL, REPRESENTATIVE, generateMember(), false));
+        profileList.add(generateProfileEntity(2L, EXPECTED_URL, REPRESENTATIVE, generateMember(), false));
+        return profileList;
+    }
+
+    private ProfileImageEntity generateProfileEntity(Long id, String imageUrl, ImageType imageType, MemberEntity member, boolean deleted) {
+        return ProfileImageEntity.builder()
+                .id(id)
+                .imageUrl(imageUrl)
+                .imageType(imageType)
+                .member(member)
+                .deleted(deleted)
+                .build();
+    }
+
+    private MemberEntity generateMember() {
+        return MemberEntity.builder()
+                .name("jickchan")
+                .email("jickchan0117@naver.com")
+                .oauthInfo(OauthInfo.builder().build())
+                .build();
+    }
+}

758ce25f3cdf87ed420a64fdb4e3f79f6080c66e
jickDo <pocard0117@naver.com>
2024-05-28 19:59:05 +0900

    fix: valid가 작동하지 않는 문제 해결

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/calendar/controller/dto/CalendarNameRequest.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/calendar/controller/dto/CalendarNameRequest.kt
@@ -6,8 +6,8 @@ import jakarta.validation.constraints.Size
 
 @Schema(title = "회원 캘린더 제목 요청")
 data class CalendarNameRequest(
+    @Schema(title = "캘린더 제목")
     @field:NotBlank(message = "캘린더 제목은 공백 일 수 없습니다")
     @field:Size(min = 1, max = 12, message = "캘린더 제목은 최소 1자에서 최대 8자입니다.")
-    @Schema(title = "캘린더 제목")
     val title: String,
 )

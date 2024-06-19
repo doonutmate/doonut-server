4f7a571b48634c1ec2451f959a6217ecd372cf87
jickDo <pocard0117@naver.com>
2024-06-19 23:45:48 +0900

    hotfix: 에러수정

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/calendar/controller/dto/CalendarResponse.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/calendar/controller/dto/CalendarResponse.kt
@@ -1,6 +1,7 @@
 package com.doonutmate.calendar.controller.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.Instant
 
 @Schema(title = "캘린더 조회 응답")
 data class CalendarResponse(
@@ -10,5 +11,6 @@ data class CalendarResponse(
     val calendarName: String,
     val firstUploadedAt: String,
     val lastUploadedAt: String,
+    val updatedAt: Instant,
     val profileImage: String?,
 )

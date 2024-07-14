c2583339aa7677e738a2e2f564f5691b8630ccb2
Yeongun Heo <52141636+yeongunheo@users.noreply.github.com>
2024-07-14 16:20:18 +0900

    fix : 하루에 이미지를 여러장 올리면 totalCount가 계속 늘어난 이슈 해결 (#75)
    * feat: CommonDateUtils.isToday 구현
    
    * feat: 이미지 업로드 시 업데이트날짜가 오늘일 때 totalCount를 업데이트하지않도록 수정

--- doonut-core/src/test/java/com/doonutmate/doonut/calendar/model/CalendarTest.java
+++ doonut-core/src/test/java/com/doonutmate/doonut/calendar/model/CalendarTest.java
@@ -10,8 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CalendarTest {
 
     @Test
-    @DisplayName("firstUploadedAt가 null이라면 totalCount, lastUploadedAt만 업데이트한다.")
-    void toUpdated_ifFirstUploadedAtIsNull() {
+    @DisplayName("totalCount가 0이라면 totalCount, firstUploadedAt, lastUploadedAt을 업데이트한다.")
+    void toUpdated_ifTotalCountIsZero() {
 
         // given
         var now = Instant.now();
@@ -20,15 +20,15 @@ class CalendarTest {
                 .memberId(10L)
                 .calendarName("부끄러운 물개님의 캘린더")
                 .firstUploadedAt(null)
-                .lastUploadedAt(now.minusSeconds(500))
+                .lastUploadedAt(null)
+                .totalCount(0)
                 .deleted(false)
                 .build();
-        var expectedTotalCount = calendar.totalCount() + 1;
         var expected = Calendar.builder()
                 .id(calendar.id())
                 .memberId(calendar.memberId())
                 .calendarName(calendar.calendarName())
-                .totalCount(expectedTotalCount)
+                .totalCount(calendar.totalCount() + 1)
                 .firstUploadedAt(now)
                 .lastUploadedAt(now)
                 .deleted(false)
@@ -42,8 +42,8 @@ class CalendarTest {
     }
 
     @Test
-    @DisplayName("firstUploadedAt가 null이 아니라면 totalCount, lastUploadedAt만 업데이트한다.")
-    void toUpdated_ifFirstUploadedAtIsNotNull() {
+    @DisplayName("lastUploadedAt가 오늘이라면 lastUploadedAt만 업데이트한다.")
+    void toUpdated_ifLastUploadedAtIsToday() {
 
         // given
         var now = Instant.now();
@@ -51,16 +51,16 @@ class CalendarTest {
                 .id(1L)
                 .memberId(10L)
                 .calendarName("부끄러운 물개님의 캘린더")
-                .firstUploadedAt(now.minusSeconds(100))
-                .lastUploadedAt(now.minusSeconds(500))
+                .firstUploadedAt(now.minusSeconds(3))
+                .lastUploadedAt(now.minusSeconds(3))
+                .totalCount(1)
                 .deleted(false)
                 .build();
-        var expectedTotalCount = calendar.totalCount() + 1;
         var expected = Calendar.builder()
                 .id(calendar.id())
                 .memberId(calendar.memberId())
                 .calendarName(calendar.calendarName())
-                .totalCount(expectedTotalCount)
+                .totalCount(calendar.totalCount())
                 .firstUploadedAt(calendar.firstUploadedAt())
                 .lastUploadedAt(now)
                 .deleted(false)
@@ -68,6 +68,39 @@ class CalendarTest {
 
         // when
         var actual = calendar.toUpdated(now);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("totalCount가 0이 아니고 lastUploadedAt가 오늘이 아니라면, totalCount, lastUploadedAt을 업데이트한다.")
+    void toUpdated_ifLastUploadedAtIsNotToday() {
+
+        // given
+        var now = Instant.now();
+        var tomorrow = now.plusSeconds(86400);
+        var calendar = Calendar.builder()
+                .id(1L)
+                .memberId(10L)
+                .calendarName("부끄러운 물개님의 캘린더")
+                .firstUploadedAt(now)
+                .lastUploadedAt(now.minusSeconds(3))
+                .totalCount(1)
+                .deleted(false)
+                .build();
+        var expected = Calendar.builder()
+                .id(calendar.id())
+                .memberId(calendar.memberId())
+                .calendarName(calendar.calendarName())
+                .totalCount(calendar.totalCount() + 1)
+                .firstUploadedAt(calendar.firstUploadedAt())
+                .lastUploadedAt(tomorrow)
+                .deleted(false)
+                .build();
+
+        // when
+        var actual = calendar.toUpdated(tomorrow);
 
         // then
         assertThat(actual).isEqualTo(expected);

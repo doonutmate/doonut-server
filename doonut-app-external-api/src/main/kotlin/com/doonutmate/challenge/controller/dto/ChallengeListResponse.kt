cf3113d8c06f141c38b8e175dbcc22f0cc5816d4
yeongun <gjduddns@gmail.com>
2024-08-20 18:43:56 +0900

    feat: 캘린더 조회 응답값에 timestamp 추가

--- doonut-app-external-api/src/main/kotlin/com/doonutmate/challenge/controller/dto/ChallengeListResponse.kt
+++ doonut-app-external-api/src/main/kotlin/com/doonutmate/challenge/controller/dto/ChallengeListResponse.kt
@@ -1,6 +1,7 @@
 package com.doonutmate.challenge.controller.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
 
 @Schema(title = "Challenge 목록 조회 응답")
 data class ChallengeListResponse(
@@ -12,4 +13,7 @@ data class ChallengeListResponse(
 
     @Schema(title = "썸네일 이미지 url")
     val thumbNailUrl: String,
+
+    @Schema(title = "업로드 시간", example = "2024-06-16T21:19:41")
+    var timestamp: LocalDateTime,
 )
